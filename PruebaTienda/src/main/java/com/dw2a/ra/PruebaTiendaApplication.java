2592f34cd532fd27cc06d9ac610733ff58df5665
AlexValero5 <58848651+AlexValero5@users.noreply.github.com>
2021-09-14 17:44:57 +0200

    Add files via upload
    ny

---
+++ PruebaTienda/src/main/java/com/dw2a/ra/PruebaTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.dw2a.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTiendaApplication.class, args);
+	}
+
+}
---
+++ PruebaTienda/src/test/java/com/dw2a/ra/PruebaTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw2a.ra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
